75c1f139f13cfabcde7b0582e3a29a4b0624163d
Rettoph <turner.anthony.d@gmail.com>
2019-01-29 12:55:17 -0700

    Enable client side velocity lerping

--- VoidHuntersRevived.Client/Drivers/ClientFarseerEntityDriver.cs
+++ VoidHuntersRevived.Client/Drivers/ClientFarseerEntityDriver.cs
@@ -12,9 +12,9 @@ namespace VoidHuntersRevived.Client.Drivers
     class ClientFarseerEntityDriver : IFarseerEntityDriver
     {
         public Vector2 Position { get; set; }
-        public Vector2 LinearVelocity { get { return _parent.Body.LinearVelocity; } set { _parent.Body.LinearVelocity = value; } }
+        public Vector2 LinearVelocity { get; set; }
         public Single Rotation { get; set; }
-        public Single AngularVelocity { get { return _parent.Body.AngularVelocity; } set { _parent.Body.AngularVelocity = value; } }
+        public Single AngularVelocity { get; set; }
 
         private IFarseerEntity _parent;
         private Single _lerpStrength = 0.5f;
@@ -29,13 +29,19 @@ namespace VoidHuntersRevived.Client.Drivers
             if (_parent.Body.Awake)
             {
                 _parent.Body.Position = Vector2.Lerp(_parent.Body.Position, this.Position, _lerpStrength);
-                //_parent.Body.LinearVelocity = Vector2.Lerp(_parent.Body.LinearVelocity, this.LinearVelocity, _lerpStrength);
+                _parent.Body.LinearVelocity = Vector2.Lerp(_parent.Body.LinearVelocity, this.LinearVelocity, _lerpStrength);
 
                 _parent.Body.Rotation = MathHelper.Lerp(_parent.Body.Rotation, this.Rotation, _lerpStrength);
-                //_parent.Body.AngularVelocity = MathHelper.Lerp(_parent.Body.AngularVelocity, this.AngularVelocity, _lerpStrength);
+                _parent.Body.AngularVelocity = MathHelper.Lerp(_parent.Body.AngularVelocity, this.AngularVelocity, _lerpStrength);
             }
             else
             {
+                _parent.Body.Position = _parent.Body.Position;
+                _parent.Body.LinearVelocity = _parent.Body.LinearVelocity;
+
+                _parent.Body.Rotation = _parent.Body.Rotation;
+                _parent.Body.AngularVelocity = _parent.Body.AngularVelocity;
+
                 _parent.SetEnabled(false);
             }
         }
