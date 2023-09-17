93872218c1d7bf4c7514c140b7e7e2145a720c6e
rettoph <anthony@turner.ec>
2023-09-17 16:26:04 -0600

    Begin work on blueprints

---
+++ src/VoidHuntersRevived.Common.Pieces/Blueprint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidHuntersRevived.Common.Pieces
+{
+    public class Blueprint
+    {
+        public Piece Piece { get; set; } = string.Empty;
+    }
+}
