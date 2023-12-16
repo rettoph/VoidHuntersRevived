93b56353438158e026d17b53193d72e9ad28f764
rettoph <anthony@turner.ec>
2023-12-15 22:17:44 -0700

    Remove setting registration

--- src/VoidHuntersRevived.Common/Constants/Settings.cs
+++ src/VoidHuntersRevived.Common/Constants/Settings.cs
@@ -9,7 +9,7 @@ namespace VoidHuntersRevived.Common.Constants
 {
     public static class Settings
     {
-        public static readonly Setting<Fix64> StepInterval = Setting.Get<Fix64>(nameof(StepInterval));
-        public static readonly Setting<int> StepsPerTick = Setting.Get<int>(nameof(StepsPerTick));
+        public static readonly Setting<Fix64> StepInterval = Setting.Get<Fix64>(nameof(StepInterval), (Fix64)20 / (Fix64)1000, "Simulation step interval in milliseconds");
+        public static readonly Setting<int> StepsPerTick = Setting.Get<int>(nameof(StepsPerTick), 3, "Number of steps taken each simulation tick");
     }
 }
