80bc1b8d83c504c8a83922c3e7bcc6cae8f62d52
Anthony Turner <anthony@turner.ec>
2024-03-10 16:07:25 -0600

    Fixes 20 - Move most Common projects to Domain.Common

---
+++ src/presentation/VoidHuntersRevived.Presentation.DummyClient/Loaders/MainLoader.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Guppy.Attributes;
+using Guppy.Common.Autofac;
+using Guppy.Extensions.Autofac;
+using Guppy.Files.Enums;
+using Guppy.Files.Helpers;
+using Guppy.Files.Providers;
+using Guppy.Game.Common;
+using Guppy.Game.Extensions.Serilog;
+using Guppy.Loaders;
+using Serilog;
+
+namespace VoidHuntersRevived.Presentation.Client.Loaders
+{
+    [AutoLoad]
+    internal class MainLoader : IServiceLoader
+    {
+        public void ConfigureServices(ContainerBuilder services)
+        {
+            services.Configure<LoggerConfiguration>((scope, config) =>
+            {
+                if (scope.HasTag(LifetimeScopeTags.GuppyScope))
+                {
+                    var fileTypePaths = scope.Resolve<IPathProvider>();
+                    var source = fileTypePaths.GetSourceLocation(DirectoryType.AppData, "logs", $"log_{DateTime.Now.ToString("yyyy-dd-M")}.txt");
+                    DirectoryHelper.EnsureDirectoryExists(source);
+
+                    config
+                        .WriteTo.File(
+                            path: source.Path,
+                            outputTemplate: "[{PeerType}][{SimulationType}][{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+                            retainedFileCountLimit: 5,
+                            shared: true
+                        )
+                        .WriteTo.Terminal(scope.Resolve<ITerminal>(), outputTemplate: "[{PeerType}][{SimulationType}][{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+                }
+            });
+        }
+    }
+}
---
+++ src/presentation/VoidHuntersRevived.Presentation.Server/Loaders/MainLoader.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Guppy.Attributes;
+using Guppy.Common.Autofac;
+using Guppy.Extensions.Autofac;
+using Guppy.Files.Enums;
+using Guppy.Files.Helpers;
+using Guppy.Files.Providers;
+using Guppy.Loaders;
+using Serilog;
+
+namespace VoidHuntersRevived.Presentation.Server.Loaders
+{
+    [AutoLoad]
+    internal sealed class MainLoader : IServiceLoader
+    {
+        public void ConfigureServices(ContainerBuilder services)
+        {
+            services.Configure<LoggerConfiguration>((scope, config) =>
+            {
+                if (scope.HasTag(LifetimeScopeTags.GuppyScope))
+                {
+                    var fileTypePaths = scope.Resolve<IPathProvider>();
+                    var source = fileTypePaths.GetSourceLocation(DirectoryType.AppData, "logs", $"log_{DateTime.Now.ToString("yyyy-dd-M")}.txt");
+                    DirectoryHelper.EnsureDirectoryExists(source);
+
+                    config
+                        .WriteTo.File(
+                            path: source.Path,
+                            outputTemplate: "[{PeerType}][{SimulationType}][{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+                            retainedFileCountLimit: 5,
+                            shared: true
+                        )
+                        .WriteTo.Console(outputTemplate: "[{PeerType}][{SimulationType}][{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+                }
+            });
+        }
+    }
+}
