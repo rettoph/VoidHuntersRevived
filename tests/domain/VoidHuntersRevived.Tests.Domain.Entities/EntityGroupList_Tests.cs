f98769df663adf7f0475ffd52ac4ae4a88033b46
rettoph <anthony@turner.ec>
2024-12-09 00:16:08 -0700

    #30 - Add EntityGroup and EntityGroupList unit tests

---
+++ tests/domain/VoidHuntersRevived.Tests.Domain.Entities/EntityGroupList_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Svelto.ECS;
+using VoidHuntersRevived.Domain.Entities.Common;
+using VoidHuntersRevived.Tests.Domain.Entities.Components;
+
+namespace VoidHuntersRevived.Tests.Domain.Entities
+{
+    public class EntityGroupList_Tests
+    {
+        private struct TestComponentTwo : IEntityComponent
+        {
+
+        }
+
+        [Fact]
+        public void EntityGroupList_AutoAddExistingGroup()
+        {
+            EntityGroup group = EntityGroup.Create("test", [typeof(TestComponent)]);
+            EntityGroupList groups = EntityGroupList.GetOrCreate([typeof(TestComponent)]);
+            Assert.Equal(1, groups.Values.count);
+        }
+
+        [Fact]
+        public void EntityGroupList_EnsureMatchingComponentsReused()
+        {
+            EntityGroup group = EntityGroup.Create("test", [typeof(TestComponent), typeof(TestComponent)]);
+
+            EntityGroupList groups1 = EntityGroupList.GetOrCreate([typeof(TestComponentTwo), typeof(TestComponent)]);
+            EntityGroupList groups2 = EntityGroupList.GetOrCreate([typeof(TestComponent), typeof(TestComponentTwo)]);
+
+            bool refsEqual = object.ReferenceEquals(groups1, groups2);
+            Assert.True(refsEqual);
+        }
+    }
+}
